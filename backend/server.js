8d7b4cb141e150e7422c1ca911ebe445d07f0e38
Tai0526 <83473679+Tai0526@users.noreply.github.com>
2023-09-26 08:39:58 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use("/api/tasks", taskRoutes);
 
-//Routes
-app.get("/", (req, res) => {
-  res.send("Home page");
-});
-
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
